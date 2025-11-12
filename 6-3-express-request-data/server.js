cf488bb764e9bd603d96cbe1ad16898481ab2870
TurkiAlslamah <s202172030@kfupm.edu.sa>
2025-11-12 08:53:01 +0300

    todo2

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -113,6 +113,14 @@ app.listen(3000, () => {
   console.log("API running at http://localhost:3000");
 });
 
+app.get("/echo", (req, res) => {
+  const { name, age } = req.query;
+   if (!name || !age) {
+      return res.status(400).json({ ok: false, error: "name & age required" });
+   }
+   res.json({ ok: true, name, age, msg: `Hello ${name}, you are ${age}` });
+});
+
 // Query params: /echo?name=Ali&age=22
 
 
